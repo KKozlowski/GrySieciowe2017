8020ca307e2bfcbbe0b71423a55e75903aa0c236
KKozlowski <septuater@gmail.com>
2017-10-03 20:31:38 +0200

    Character movement and shooting. Some debug behaviors.

---
+++ client/Assets/Scripts/Gameplay/CharacterController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	public static CharacterController Player { get; private set; }
+    public LineRenderer laser;
+
+    private void Awake() {
+        //To be updated when there are more players
+        Player = this;
+        
+    }
+
+    private void Start() {
+        laser.startColor = new Color(1, 1, 1, 0);
+        laser.endColor = new Color(1, 1, 1, 0);
+    }
+
+    public void MoveTo(Vector2 newPosition) {
+        transform.position = newPosition;
+    }
+
+    public void Shoot(Vector2 direction) {
+        laser.SetPositions(
+            new Vector3[] { direction * 0.2f, direction * 100f }
+            );
+        StartCoroutine(shootEffect());
+    }
+
+    public void Die() {
+        Debug.Log("<dying sounds>");
+    }
+
+    private uint currentShootId = 0;
+
+    private IEnumerator shootEffect() {
+        Color startColor = new Color(1, 1, 1, 1);
+        Color endColor = new Color(1, 1, 1, 0);
+
+        uint thisShootId = ++currentShootId;
+
+        float timeRequired = 0.3f, timePassed = 0;
+        while(timePassed < timeRequired && currentShootId == thisShootId) {
+            laser.startColor = laser.endColor = Color.Lerp(startColor, endColor, timePassed / timeRequired);
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+
+        if (currentShootId == thisShootId)
+            laser.startColor = laser.endColor = endColor;
+    }
+}
---
+++ client/Assets/Scripts/Gameplay/PlayerInput.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+    Plane ground;
+
+    private void Awake() {
+        ground = new Plane(Vector3.back, Vector3.zero);
+    }
+
+    void Update () {
+        UpdateMovement();
+
+        if (Input.GetMouseButtonDown(0)) {
+            UpdateShot(Input.mousePosition);
+        }
+    }
+
+    private void UpdateMovement() {
+        Vector2 direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W)) {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            direction += Vector2.right;
+        }
+
+        direction.Normalize();
+
+        Move(direction);
+    }
+
+    private void Move(Vector2 direction) {
+
+        #if DEBUG_SHIT
+        if (CharacterController.Player)
+            CharacterController.Player
+                .MoveTo((Vector2)CharacterController.Player.transform.position 
+                        + direction * Time.deltaTime*2);
+        #endif
+    }
+
+    private void UpdateShot(Vector3 pointerPosition) {
+        if (CharacterController.Player == null)
+            return;
+
+        Vector2 playerPosition = CharacterController.Player.transform.position;
+        Ray cameraRay = Camera.main.ScreenPointToRay(pointerPosition);
+        float rayDist = 0;
+        ground.Raycast(cameraRay, out rayDist);
+
+        Vector2 targetPosition = cameraRay.origin + cameraRay.direction * rayDist;
+
+        Vector2 direction = (targetPosition - playerPosition).normalized;
+        Shoot(direction);
+    }
+
+    private void Shoot(Vector2 direction) {
+        Debug.DrawRay(CharacterController.Player.transform.position, direction);
+#if DEBUG_SHIT
+        if (CharacterController.Player)
+            CharacterController.Player
+                .Shoot(direction);
+        #endif
+    }
+}
