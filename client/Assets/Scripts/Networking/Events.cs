a86a6fae057e76e5b12f3574e090981050e98e50
Luke Nizik <lukasz@superhotgame.com>
2017-10-30 19:02:28 +0100

    * EventsFactory can now create events using static event id value

--- client/Assets/Scripts/Networking/Events.cs
+++ client/Assets/Scripts/Networking/Events.cs
@@ -12,6 +12,7 @@ public abstract class EventBase
 {
     public abstract void Deserialize( ByteStreamReader reader );
     public abstract void Serialize( ByteStreamWriter writer );
+    public abstract byte GetId();
 }
 
 public class TestEvent : EventBase
@@ -23,11 +24,21 @@ public class TestEvent : EventBase
     public override void Deserialize( ByteStreamReader reader )
     {
     }
+
+    public override byte GetId()
+    {
+        return (byte)EventType.HelloWorld;
+    }
+
+    public static byte GetStaticId()
+    {
+        return ( byte )EventType.HelloWorld;
+    }
 }
 
 public class EventsFactory
 {
-    FastMap<Type> m_events;
+    FastMap<Type> m_events = new FastMap<Type>();
 
     public EventsFactory()
     {
@@ -37,6 +48,20 @@ public class EventsFactory
     void Init()
     {
         List<Type> eventClasses = GetEventClasses();
+
+        int count = eventClasses.Count;
+
+        MethodInfo method;
+        for ( int i = 0; i < count; ++i )
+        {
+            method = eventClasses[i].GetMethod( "GetStaticId", BindingFlags.Public | BindingFlags.Static );
+            System.Diagnostics.Debug.Assert( method != null );
+            System.Diagnostics.Debug.Assert( method.ReturnType == typeof( byte ) );
+
+            byte id = (byte)method.Invoke( eventClasses[i], null );
+            m_events.AddUnique( id, eventClasses[i] );
+        }
+        MethodInfo methodInfo = eventClasses[0].GetMethod( "GetID", BindingFlags.Static );
     }
 
     List< Type > GetEventClasses()
@@ -55,6 +80,17 @@ public class EventsFactory
             }
         }
 
+        return result;
+    }
+
+    public EventBase CreateEvent( int id )
+    {
+        EventBase result;
+        Type eventType = m_events.Find( id );
+        System.Diagnostics.Debug.Assert( eventType != null, "Event type not found id: " + id );
+        System.Diagnostics.Debug.Assert( eventType.IsSubclassOf( typeof( EventBase ) ), "Event doesn't inherit from EventBase id: " + id );
+
+        result = (EventBase)Activator.CreateInstance( eventType );
         return result;
     }
 }
