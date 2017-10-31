995d7418fc818f9d116b37ad65c4e3ab651f4408
Luke Nizik <lukasz@superhotgame.com>
2017-10-31 18:53:57 +0100

    # fixed Unity build

--- client/Assets/Scripts/PlayerConnection.cs
+++ client/Assets/Scripts/PlayerConnection.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class PlayerConnection : MonoBehaviour
 {
-    Client m_client;
+    NetClient m_client;
     Thread m_connectThread;
 
     public void Start()
     {
-        m_client = new Client();
+        m_client = new NetClient();
         m_connectThread = new Thread( () => m_client.Connect( "127.0.0.1", 1111 ) );
         m_connectThread.Start();
     }
